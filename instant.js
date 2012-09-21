841f93efb81f3952529bf2be5080e34147484d19
China Life <634844533@qq.com>
2012-09-21 10:33:43 +0800

    initial committed
    sogou.com

---
+++ instant.js
@@ -0,0 +1,16 @@
+$(function(){
+	//category search hover
+		$('.catetabfaq').hover(function(){
+			$('.cateintro').show();
+		},function(){
+			$('.cateintro').hide();
+		});
+
+		$('#navibtn').click(function(){
+
+			$('#navimore').show();
+		})
+   
+
+
+})
